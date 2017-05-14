59f45b8cba5d845ccb7d8974378656befa18dc57
lhepto <lheptonstall@live.co.uk>
2017-05-14 20:04:24 +0100

    scale and centered R,G,B,IR in input images changed to ADAM updates method changed to leaky_rectify now training on a batch_size as large as possible seems to converge to 90% accuracy, loss function decreases

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from skimage import io
 from os import walk
 import csv
 import sys
-import os
+import sklearn.preprocessing as pp
 import time
 
 import numpy as np
@@ -24,8 +24,10 @@ def get_labels(no_images, path):
             row_iterator += 1
 
     # Find all the labels
-    all_labels = np.unique(
-        np.asarray(([item for sublist in training_data for item in sublist])));  # flattens all label data
+    #all_labels = np.unique(
+    #    np.asarray(([item for sublist in training_data for item in sublist])));  # flattens all label data
+
+    all_labels = np.load("labels.npy")
 
     # Build a large matrix IMAGES X TAG CODE
     training_data_matrix = np.zeros((no_images, all_labels.shape[0]), dtype=np.uint16)
@@ -49,11 +51,17 @@ def get_image_data(no_image, directory):
         break
 
     # Use skimage to put all the training data into a big array which is IMAGES,LEFT,RIGHT,CHANNEL
-    training_image_data = np.zeros((no_image, 256, 256, 4), dtype=np.uint16)
+    training_image_data = np.zeros((no_image, 256, 256, 4), dtype=np.double)
     image_iterator = 0
     for training_image in training_images:
         if (image_iterator < no_image):
-            training_image_data[image_iterator, :, :, :] = (io.imread(directory+training_image))
+            raw_data = (io.imread(directory+training_image))
+            color_channel_means = np.mean(np.mean(raw_data,axis=0),axis=0)
+            color_channel_stdevs = np.std(np.std(raw_data,axis=0),axis=0)
+
+            raw_data = (raw_data - color_channel_means)/color_channel_stdevs # SCALE R,G,B,IR to mean 0 and stddev 1
+
+            training_image_data[image_iterator, :, :, :] = raw_data
             image_iterator += 1
 
     training_image_data = np.swapaxes(training_image_data,1,3)
@@ -75,9 +83,9 @@ def build_cnn(input_var=None):
     # Convolutional layer with 32 kernels of size 5x5. Strided and padded
     # convolutions are supported as well; see the docstring.
     network = lasagne.layers.Conv2DLayer(
-            network, num_filters=10, filter_size=(5, 5),
-            nonlinearity=lasagne.nonlinearities.rectify,
-            W=lasagne.init.GlorotUniform())
+           network, num_filters=10, filter_size=(5, 5),
+           nonlinearity=lasagne.nonlinearities.leaky_rectify,
+           W=lasagne.init.GlorotUniform())
     # Expert note: Lasagne provides alternative convolutional layers that
     # override Theano's choice of which implementation to use; for details
     # please see http://lasagne.readthedocs.org/en/latest/user/tutorial.html.
@@ -88,24 +96,29 @@ def build_cnn(input_var=None):
     # Another convolution with 32 5x5 kernels, and another 2x2 pooling:
     network = lasagne.layers.Conv2DLayer(
             network, num_filters=32, filter_size=(5, 5),
-            nonlinearity=lasagne.nonlinearities.rectify)
+            nonlinearity=lasagne.nonlinearities.leaky_rectify)
+
+
+
     network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
+
+
 
     # A fully-connected layer of 256 units with 50% dropout on its inputs:
     network = lasagne.layers.DenseLayer(
             lasagne.layers.dropout(network, p=.5),
             num_units=256,
-            nonlinearity=lasagne.nonlinearities.rectify)
+            nonlinearity=lasagne.nonlinearities.leaky_rectify)
 
     # And, finally, the 10-unit output layer with 50% dropout on its inputs:
     network = lasagne.layers.DenseLayer(
             lasagne.layers.dropout(network, p=.5),
             num_units=17,
-            nonlinearity=lasagne.nonlinearities.softmax)
+            nonlinearity=lasagne.nonlinearities.sigmoid)
 
     return network
 
-# ############################# Batch iterator ###############################
+0# ############################# Batch iterator ###############################
 # This is just a simple helper function iterating over training data in
 # mini-batches of a particular size, optionally in random order. It assumes
 # data is available as numpy arrays. For big datasets, you could load numpy
@@ -133,24 +146,24 @@ def main():
 
     train_images = 200
     test_images = 100
-    num_epochs = 10
+    num_epochs = 20
 
     PLANET_KAGGLE_ROOT = "B:/rainforest-kaggle/"
     TRAIN_SET = "lewis-data/mini/train/"
     TEST_SET = "lewis-data/mini/test/"
 
     # Generate Training Data from Files - Save to Numpy File
-    # y_train = get_labels(train_images,PLANET_KAGGLE_ROOT+"lewis-data/mini/train.csv")
-    # X_train = get_image_data(train_images,PLANET_KAGGLE_ROOT+TRAIN_SET)
-    #
-    # y_test = get_labels(test_images,PLANET_KAGGLE_ROOT+"lewis-data/mini/test.csv")
-    # X_test = get_image_data(test_images,PLANET_KAGGLE_ROOT+TEST_SET)
-    #
-    # np.save("training_image_data",X_train)
-    # np.save("training_labels_data",y_train)
-    #
-    # np.save("test_image_data",X_test)
-    # np.save("test_labels_data",y_test)
+    y_train = get_labels(train_images,PLANET_KAGGLE_ROOT+"lewis-data/mini/train.csv")
+    X_train = get_image_data(train_images,PLANET_KAGGLE_ROOT+TRAIN_SET)
+
+    y_test = get_labels(test_images,PLANET_KAGGLE_ROOT+"lewis-data/mini/test.csv")
+    X_test = get_image_data(test_images,PLANET_KAGGLE_ROOT+TEST_SET)
+
+    np.save("training_image_data",X_train)
+    np.save("training_labels_data",y_train)
+
+    np.save("test_image_data",X_test)
+    np.save("test_labels_data",y_test)
 
     # Quick load the data
     X_train = np.load("training_image_data.npy")
@@ -163,13 +176,13 @@ def main():
 
     # Prepare Theano variables for inputs and targets
     input_var = T.tensor4('inputs')
-    target_var = T.matrix('targets')
+    target_var = T.imatrix('targets')
 
     network = build_cnn(input_var)
 
     # Create a loss expression for training, i.e., a scalar objective we want
     # to minimize (for our multi-class problem, it is the cross-entropy loss):
-    prediction = T.clip(lasagne.layers.get_output(network),0.001,0.999)
+    prediction = lasagne.layers.get_output(network)
     loss = lasagne.objectives.binary_crossentropy(prediction, target_var)
     loss = loss.mean()
     # We could add some weight decay as well here, see lasagne.regularization.
@@ -178,13 +191,13 @@ def main():
     # parameters at each training step. Here, we'll use Stochastic Gradient
     # Descent (SGD) with Nesterov momentum, but Lasagne offers plenty more.
     params = lasagne.layers.get_all_params(network, trainable=True)
-    updates = lasagne.updates.nesterov_momentum(
-            loss, params, learning_rate=0.01, momentum=0.9)
+    updates = lasagne.updates.adam(
+            loss, params, learning_rate=0.001)
 
     # Create a loss expression for validation/testing. The crucial difference
     # here is that we do a deterministic forward pass through the network,
     # disabling dropout layers.
-    test_prediction = T.clip(lasagne.layers.get_output(network, deterministic=True),0.001,0.999)
+    test_prediction = lasagne.layers.get_output(network, deterministic=True)
     test_objective = lasagne.objectives.binary_crossentropy(test_prediction, target_var)
     test_loss = test_objective.mean()
 
@@ -195,8 +208,8 @@ def main():
     train_fn = theano.function([input_var, target_var], loss, updates=updates)
 
     # Compile a second function computing the validation loss and accuracy:
-    test_acc = T.mean(T.eq(T.round_half_away_from_zero(test_prediction), target_var), dtype=theano.config.floatX)
-    training_fn = theano.function([input_var, target_var], [test_loss, test_acc])
+    training_acc = T.mean(T.eq(T.round_half_away_from_zero(test_prediction), target_var), dtype=theano.config.floatX)
+    training_fn = theano.function([input_var, target_var], [test_loss, training_acc])
 
     print("Starting training...")
     # We iterate over epochs:
@@ -205,7 +218,7 @@ def main():
         train_err = 0
         train_batches = 0
         start_time = time.time()
-        for batch in iterate_minibatches(X_train, y_train, 10, shuffle=True):
+        for batch in iterate_minibatches(X_train, y_train, 200, shuffle=True):
             inputs, targets = batch
             train_err += train_fn(inputs, targets)
             train_batches += 1
@@ -213,11 +226,14 @@ def main():
          # Then we print the results for this epoch:
         print("Epoch {} of {} took {:.3f}s".format(
             epoch + 1, num_epochs, time.time() - start_time))
-        print("  training loss:\t\t{:.6f}".format(train_err / train_batches))
 
-        err, acc = training_fn(X_train, y_train)
-        print("  total training loss:\t\t\t{:.6f}".format(err))
-        print("  total training acc:\t\t{:.6f}".format(acc))
+        testErr, testAcc = training_fn(X_test, y_test)
+        trainingErr, trainingAcc = training_fn(X_train, y_train)
+
+        print("TRAINING: loss: \t\t{!s:} accuracy: {!s:}".format(trainingErr, trainingAcc))
+        print("TEST: loss: \t\t{!s:} accuracy: {!s:}".format(testErr, testAcc))
+
+    print(predict_fn(X_train))
 
 
 
